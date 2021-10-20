c782d682f219fe71643e92205843c4840ad422cd
GabrielTomonari <gabrieltomonari@gmail.com>
2021-10-20 10:19:09 -0300

    initial commit

---
+++ omr.py
@@ -0,0 +1,223 @@
+# Import modules
+import cv2
+import numpy as np
+from numpy.core.fromnumeric import std
+from numpy.lib.function_base import average
+import utils
+import beepy
+
+cap = cv2.VideoCapture(0)
+count = 0
+imgHeight = 1920
+imgWidth = 1080
+products = 25
+choices = 10
+timer = 0
+lastDetected = False
+productsList=[
+    "Água 1,5 L",
+    "Água com gás",
+    "Água sem gás",
+    "Água tônica",
+    "Cerveja 600 mL Comum",
+    "Cerveja 600 mL Premium",
+    "Cerveja Lata",
+    "Cerveja Long Neck",
+    "H2OH",
+    "Marmitex com churrasco",
+    "Marmitex sem churrasco",
+    "Picolé grupo 1",
+    "Picolé grupo 2",
+    "Picolé grupo 3",
+    "Picolé grupo 4",
+    "Red Bull",
+    "Refrigerante 1 L",
+    "Refrigerante 1,5 L",
+    "Refrigerante 2 L",
+    "Refrigerante 600 mL",
+    "Refrigerante KS",
+    "Refrigerante Lata",
+    "Refrigerante NS",
+    "Suco Copo",
+    "Suco Jarra"
+]
+priceList=[
+    6.0,
+    4.0,
+    3.0,
+    5.0,
+    13.0,
+    16.0,
+    5.0,
+    7.0,
+    5.0,
+    18.0,
+    19.0,
+    7.0,
+    4.0,
+    9.0,
+    3.0,
+    12,
+    8,
+    10,
+    12,
+    6,
+    4.5,
+    5,
+    5.5,
+    5,
+    16
+]
+
+while(1):
+    ret, frame = cap.read()
+    if frame is not None:
+        img = frame
+    else: img = cv2.imread("comanda2.jpg")
+    img = cv2.resize(img, (imgHeight, imgWidth))
+    #img = utils.increase_brightness(img,40)
+    #img = utils.increase_contrast(img)
+    imgRotated = cv2.rotate(img,cv2.ROTATE_90_CLOCKWISE)
+    # CREATE A BLANK IMAGE FOR TESTING DEBUGING IF REQUIRED
+    imgBlank = np.zeros((imgWidth, imgHeight, 3), np.uint8)
+    # CONVERT IMAGE TO GRAY SCALE
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (79, 79), 3)  # ADD GAUSSIAN BLUR
+    high_thresh, thresh_im = cv2.threshold(imgBlur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    lowThresh = 0.5*high_thresh
+    imgThreshold = cv2.Canny(imgBlur, lowThresh, high_thresh)  # APPLY CANNY BLUR
+    kernel = np.ones((5, 5))
+    imgDial = cv2.dilate(imgThreshold, kernel, iterations=2)  # APPLY DILATION
+    imgThreshold = cv2.erode(imgDial, kernel, iterations=1)  # APPLY EROSION
+
+    # FIND ALL COUNTOURS
+    imgContours = img.copy()  # COPY IMAGE FOR DISPLAY PURPOSES
+    imgBigContour = img.copy()  # COPY IMAGE FOR DISPLAY PURPOSES
+    contours, hierarchy = cv2.findContours(
+        imgThreshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # FIND ALL CONTOURS
+    cv2.drawContours(imgContours, contours, -1, (0, 255, 0),
+                    10)  # DRAW ALL DETECTED CONTOURS
+
+    # FIND THE BIGGEST COUNTOUR
+    biggest, maxArea = utils.biggestContour(
+        contours)  # FIND THE BIGGEST CONTOUR
+    if biggest.size != 0:
+            if lastDetected == True and maxArea > 400000:
+                timer+=1
+                #print("Detectando bordas"+timer*"-")
+            else:
+                timer=0
+            biggest = utils.reorder(biggest)
+            # DRAW THE BIGGEST CONTOUR
+            cv2.drawContours(imgBigContour, biggest, -1, (0, 255, 0), 20)
+            imgBigContour = utils.drawRectangle(imgBigContour, biggest, 2)
+            pts1 = np.float32(biggest)  # PREPARE POINTS FOR WARP
+            # PREPARE POINTS FOR WARP
+            pts2 = np.float32([[0, 0], [imgHeight, 0], [0, imgWidth], [imgHeight, imgWidth]])
+            matrix = cv2.getPerspectiveTransform(pts1, pts2)
+            imgWarpColored = cv2.warpPerspective(img, matrix, (imgHeight, imgWidth))
+            imgWarpColoredRotated = cv2.rotate(imgWarpColored,cv2.ROTATE_90_CLOCKWISE)
+
+            # REMOVE 20 PIXELS FORM EACH SIDE
+            imgWarpColored = imgWarpColored[20:imgWarpColored.shape[0] - 20, 20:imgWarpColored.shape[1] - 20]
+            imgWarpColored = cv2.resize(imgWarpColored, (imgHeight, imgWidth))
+            boxesCountoursList = [[210,45],[210,590],[1670,590],[1670,45]]
+            ctr = np.array(boxesCountoursList).reshape((-1,1,2)).astype(np.int32)
+            cv2.drawContours(imgWarpColored,[ctr],-1,(0,255,0),2)
+
+            boxesArea = imgWarpColored.copy()
+            boxesArea = boxesArea[45:590, 210:1670]
+            boxesArea = cv2.resize(boxesArea,(imgHeight,imgWidth))
+
+            # APPLY ADAPTIVE THRESHOLD
+            boxesAreaGray = cv2.cvtColor(boxesArea, cv2.COLOR_BGR2GRAY)
+            boxesAreaTresh = cv2.threshold(boxesAreaGray,90, 255, cv2.THRESH_BINARY_INV)[1]
+            boxesAreaTreshRotated = cv2.rotate(boxesAreaTresh,cv2.ROTATE_90_CLOCKWISE)
+            boxes=utils.splitBoxes(boxesAreaTreshRotated)
+            
+
+            # COUNT NON ZERO PIXELS
+
+            myPixelVal = np.zeros((products,choices))
+            countC=0
+            countR=0
+
+            for box in boxes:
+                totalPixels = cv2.countNonZero(box)
+                myPixelVal[countR][countC] = totalPixels
+                countC+=1
+                if (countC==choices):
+                    countR+=1
+                    countC=0
+            #print(myPixelVal)
+                
+            #
+            #print("-----------------------")
+            myChoices=[]
+            myGeneralAverge = average(myPixelVal)
+            myGeneralDeviation = std(myPixelVal)
+            myGeneralAnswerThreshold = myGeneralDeviation*2
+            myGeneralUpperLimit = myGeneralAverge+myGeneralAnswerThreshold
+            #print("General Average:"+str(myGeneralAverge))
+            #print("General Deviation:"+str(myGeneralDeviation))
+
+            for x in range(0,products):
+                arr = myPixelVal[x]
+                myAverage = average(arr)
+                myDeviation = std(arr)
+                myAnswerThreshold = myDeviation * 3
+                lowerLimit = myAverage - myAnswerThreshold
+                upperLimit = myAverage + myAnswerThreshold
+                answers = []
+                for y in arr:
+                    if (y>=upperLimit or y>=myGeneralUpperLimit):
+                        answers.append(1)
+                    else:
+                        answers.append(0)
+                myChoices.append(answers)
+            
+
+            imgWarpGray = cv2.cvtColor(imgWarpColored, cv2.COLOR_BGR2GRAY)
+            imgAdaptiveThre = cv2.adaptiveThreshold(imgWarpGray,255, 1, 1, 11, 2)
+            imgAdaptiveThre = cv2.bitwise_not(imgAdaptiveThre)
+            imgAdaptiveThre = cv2.medianBlur(imgAdaptiveThre, 3)
+
+            #cv2.imshow("Tresh", boxesAreaTreshRotated)
+            # Image Array for Display
+            lastDetected = True
+            imageArray = ([img, imgGray, imgThreshold, imgContours],
+                        [imgBigContour, imgWarpColored, imgWarpGray, imgAdaptiveThre],[boxesArea,boxesAreaTresh,imgBlur,imgBlank])
+
+    else:
+            lastDetected = False
+            timer=0
+            imageArray = ([img, imgGray, imgThreshold, imgContours],
+                       [imgBlank, imgBlank, imgBlank, imgBlank])
+
+        # LABELS FOR DISPLAY
+    # lables = [["Original", "Gray", "Threshold", "Contours", "Biggest Contour",
+    #             "Warp Prespective", "Warp Gray", "Adaptive Threshold"]]
+
+    stackedImage = utils.stackImages(imageArray, 1)
+    imgBigContour = cv2.rotate(imgBigContour,cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+    cv2.imshow("Result", imgRotated)
+    if (timer>=7):
+        print('\a')
+        #print (np.matrix(myChoices))
+        cv2.destroyAllWindows()
+        imgWarpColoredRotated = utils.show_choices(imgWarpColoredRotated,myChoices)
+        cv2.imshow("Scan", imgWarpColoredRotated)
+        #beepy.beep(sound="ping")
+        qtd = utils.count_total(myChoices)
+        utils.totalPrice(qtd,productsList,priceList)
+        break
+
+
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.waitKey(0)
+
---
+++ utils.py
@@ -0,0 +1,168 @@
+import cv2
+import numpy as np
+
+## TO STACK ALL THE IMAGES IN ONE WINDOW
+def stackImages(imgArray,scale,lables=[]):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+            hor_con[x] = np.concatenate(imgArray[x])
+        ver = np.vstack(hor)
+        ver_con = np.concatenate(hor)
+    else:
+        for x in range(0, rows):
+            imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        hor_con= np.concatenate(imgArray)
+        ver = hor
+    if len(lables) != 0:
+        eachImgWidth= int(ver.shape[1] / cols)
+        eachImgHeight = int(ver.shape[0] / rows)
+        #print(eachImgHeight)
+        for d in range(0, rows):
+            for c in range (0,cols):
+                cv2.rectangle(ver,(c*eachImgWidth,eachImgHeight*d),(c*eachImgWidth+len(lables[d][c])*13+27,30+eachImgHeight*d),(255,255,255),cv2.FILLED)
+                cv2.putText(ver,lables[d][c],(eachImgWidth*c+10,eachImgHeight*d+20),cv2.FONT_HERSHEY_COMPLEX,0.7,(255,0,255),2)
+    return ver
+
+def reorder(myPoints):
+
+    myPoints = myPoints.reshape((4, 2))
+    myPointsNew = np.zeros((4, 1, 2), dtype=np.int32)
+    add = myPoints.sum(1)
+
+    myPointsNew[0] = myPoints[np.argmin(add)]
+    myPointsNew[3] =myPoints[np.argmax(add)]
+    diff = np.diff(myPoints, axis=1)
+    myPointsNew[1] =myPoints[np.argmin(diff)]
+    myPointsNew[2] = myPoints[np.argmax(diff)]
+
+    return myPointsNew
+
+
+def biggestContour(contours):
+    biggest = np.array([])
+    max_area = 0
+    for i in contours:
+        area = cv2.contourArea(i)
+        if area > 5000:
+            peri = cv2.arcLength(i, True)
+            approx = cv2.approxPolyDP(i, 0.02 * peri, True)
+            if area > max_area and len(approx) == 4:
+                biggest = approx
+                max_area = area
+    return biggest,max_area
+def drawRectangle(img,biggest,thickness):
+    cv2.line(img, (biggest[0][0][0], biggest[0][0][1]), (biggest[1][0][0], biggest[1][0][1]), (0, 255, 0), thickness)
+    cv2.line(img, (biggest[0][0][0], biggest[0][0][1]), (biggest[2][0][0], biggest[2][0][1]), (0, 255, 0), thickness)
+    cv2.line(img, (biggest[3][0][0], biggest[3][0][1]), (biggest[2][0][0], biggest[2][0][1]), (0, 255, 0), thickness)
+    cv2.line(img, (biggest[3][0][0], biggest[3][0][1]), (biggest[1][0][0], biggest[1][0][1]), (0, 255, 0), thickness)
+
+    return img
+
+def nothing(x):
+    pass
+
+def initializeTrackbars(intialTracbarVals=0):
+    cv2.namedWindow("Trackbars")
+    cv2.resizeWindow("Trackbars", 360, 240)
+    cv2.createTrackbar("Threshold1", "Trackbars", 200,255, nothing)
+    cv2.createTrackbar("Threshold2", "Trackbars", 200, 255, nothing)
+
+
+def valTrackbars():
+    Threshold1 = cv2.getTrackbarPos("Threshold1", "Trackbars")
+    Threshold2 = cv2.getTrackbarPos("Threshold2", "Trackbars")
+    src = Threshold1,Threshold2
+    return src
+
+def splitBoxes(img):
+    rows=np.array_split(img,25)
+    boxes=[]
+    for r in rows:
+        
+        cols = np.hsplit(r,10)
+        
+        for box in cols:
+            boxes.append(box)
+            
+    return boxes
+
+def increase_brightness(img, value=30):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+
+    lim = 255 - value
+    v[v > lim] = 255
+    v[v <= lim] += value
+
+    final_hsv = cv2.merge((h, s, v))
+    img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+    return img
+
+def increase_contrast(img):
+
+    lab= cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+
+    #-----Splitting the LAB image to different channels-------------------------
+    l, a, b = cv2.split(lab)
+    #cv2.imshow('l_channel', l)
+    #cv2.imshow('a_channel', a)
+    #cv2.imshow('b_channel', b)
+
+    #-----Applying CLAHE to L-channel-------------------------------------------
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    cl = clahe.apply(l)
+
+    #-----Merge the CLAHE enhanced L-channel with the a and b channel-----------
+    limg = cv2.merge((cl,a,b))
+
+    #-----Converting image from LAB Color model to RGB model--------------------
+    final = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+    return final
+
+def show_choices(img,choices):
+    xStart=500
+    yStart=230
+    sqrSize=35
+    xOffset=53
+    yOffset=58
+
+    for (x, y), element in np.ndenumerate(np.array(choices)):
+        if (element==1):
+            startingPointX = xStart+xOffset*y
+            startingPointY = yStart+yOffset*x
+            cv2.rectangle(img,(startingPointX,startingPointY),(startingPointX+sqrSize,startingPointY+sqrSize),(0,255,0),-1)
+
+    return img
+
+def count_total(choices):
+    qtd=[]
+    for row in choices:
+        qtd.append(sum(row))
+    return qtd
+
+def totalPrice(qtd,productsList,priceList):
+    total=0
+    print("QTD   Produto                    Total\n")
+    for x,i in enumerate(qtd):
+        if i != 0:
+            print(" "+str(i)+"    "+productsList[x]+((28-len(productsList[x]))*" ")+str(priceList[x]*i)+"\n")
+            total=total+priceList[x]*i
+    print("Total: ",total)
+    
+
+
